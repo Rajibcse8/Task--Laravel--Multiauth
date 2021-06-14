c51ced3cd6aca9e5011962ee6ec2c2666b6d5b9f
Rajibcse8 <rajibtalukder119@gmail.com>
2021-06-14 23:53:21 +0600

    USer Role model create and Admin data insert Using Seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>'Myadmin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('12345678'),
+            'role_id'=>'1'
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Role::create([
+           'name'=>'admin'
+        ]);
+        Role::create([
+            'name'=>'merchant'
+        ]);
+        Role::create([
+            'name'=>'officer'
+        ]);
+        Role::create([
+            'name'=>'user'
+        ]);
+
         // $this->call(UserSeeder::class);
     }
 }
